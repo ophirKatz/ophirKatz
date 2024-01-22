98678430a0bdfeb1ba11e4e4466f9fbfc06b6986
Ophir Katz <ophirk8396@gmail.com>
2024-01-22 19:49:19 +0200

    choosing color palette

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,52 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   standalone: true,
   imports: [RouterOutlet],
   template: `
-    <h1>Welcome to {{title}}!</h1>
+    <div class="container">
+      <div class="gradient1"></div>
+      <div class="gradient2"></div>
+      <div class="gradient3"></div>
+      <div class="gradient4"></div>
 
-    <router-outlet />
+      <div class="circle"></div>
+
+      <router-outlet />
+    </div>
   `,
-  styles: [],
+  styles: [
+    `
+      :host {
+        padding: 50px 200px 0px 200px;
+      }
+
+      .container {
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        gap: 20px;
+        // position: relative;
+
+        > * {
+          // position: absolute;
+        }
+      }
+
+      .container > div {
+        width: 400px;
+        aspect-ratio: 1;
+
+        &.circle {
+          width: 100px;
+          top: 20px;
+          left: 20px;
+        }
+      }
+    `,
+  ],
 })
 export class AppComponent {
-  title = 'portfolio';
+  title = "portfolio";
 }
